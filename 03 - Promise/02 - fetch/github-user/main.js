de6d4a4833b48bc72b9d97c2761fb4857bca0141
anshusaurav <anshu.saurav@gmail.com>
2020-04-02 09:35:35 +0530

    'check'

--- 03 - Promise/02 - fetch/github-user/main.js
+++ 03 - Promise/02 - fetch/github-user/main.js
@@ -1,10 +1,13 @@
 // Your code goes here
 let p = document.createElement('p');
 document.body.append(p);
-let name='vivekbrh01';
+let name='nnnkit';
 let mUser = {};
 const repoList = [];
 const followerList = [];
+const resultElem = document.querySelector('.result-user');
+const searchinputElem = document.querySelector('#searchinput');
+searchinputElem.addEventListener('keydown', process);
 function getUser(uName) {
     let job = fetch(`https://api.github.com/users/${uName}`).then(
         successResponse => {
@@ -35,6 +38,43 @@ function displayUser(uName){
         mUser.following = user.following;
         mUser.public_repos = user.public_repos;
         console.log(mUser); 
+        return mUser;
+    }).then(us=>{
+        resultElem.innerHTML = `<div class='user-image'>
+        <img class="profile-img" alt=${us.name} src=${us.avatar_url}/>
+      </div>
+      <div class='user-info'>
+        <div>
+          <h2>${us.name}<span>@${us.login}</span></h2> 
+        </div>
+        <h3>${us.bio}</h3>
+        <div class='user-info-flex'>
+          <i class="fas fa-user-friends"></i>
+          <p>${us.company}</p>
+        </div>
+        <div class='user-info-flex'>
+          <i class="fas fa-map-marker-alt"></i>
+          <p>${us.location}</p>
+        </div>
+        <div class='user-info-flex'>
+          <i class="fab fa-microblog"></i>
+          <a href=${us.blog}>${us.blog}/</a>
+        </div>
+        <div class='user-data'>
+          <div>
+            <h4>${us.followers}</h4>
+            <p>Followers</p>
+          </div>
+          <div>
+            <h4>${us.following}</h4>
+            <p>Following</p>
+          </div>
+          <div>
+            <h4>${us.public_repos}</h4>
+            <p>Repos</p>
+          </div>
+        </div>
+      </div>`
     });
 }
 function getUserFollowers(uName) {
@@ -85,11 +125,13 @@ function getUserRepos(uName) {
    return job;
 }
 function displayRepos(uName){
+    console.log(getUserRepos(uName));
     getUserRepos(uName).then(repos =>{
         repos.forEach(repo =>{
             let obj = Object.create(null);
             obj.name = repo.name;
-            obj.forks_url = repo.forks_url;
+            obj.html_url = repo.html_url;
+            obj.forks_url = repo.url;
             obj.description = repo.description;
             obj.stargazers_count = repo.stargazers_count;
             obj.forks = repo.forks;
@@ -102,10 +144,13 @@ function displayRepos(uName){
         console.log(rep);
     })
 }
-displayUser(name);
+
 displayFollowers(name);
 displayRepos(name);
-
-function build(arr){
-    p.textContent = arr;
+function process(event){
+    if(event.keyCode == 13){
+        name = this.value;
+        displayUser(name);
+        this.value = '';
+    }
 }
